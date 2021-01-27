09e8714c73d65336e831867436e89ef918fff4f0
percylechat <44782706+percylechat@users.noreply.github.com>
2021-01-27 18:35:11 +0100

    now with classes

--- imc.py
+++ imc.py
@@ -1,10 +1,30 @@
+class Male:
+    def __init__(self, weight : int, size : float):
+        self.weight = weight
+        self.size = size
+    def calc(self) -> float:
+        result : float = self.weight / (self.size * self.size)
+        return result
+
+class Female:
+    def __init__(self, weight : int, size : float):
+        self.weight = weight - 10
+        self.size = size
+    def calc(self) -> float:
+        result : float = self.weight / (self.size * self.size)
+        return result
+
 def imc_calc(sex : int =2, weight : int =60, size :float =1.70):
-    value : int = 0
+    #value : int = 0
     if sex == 1:
-        value = weight / (size * size)
+        #value = weight / (size * size)
+        user = Male(weight, size)
+        print("Your imc is " + str(user.calc()))
     if sex == 2:
-        value = (weight - 10) / (size * size)
-    print("Your imc is " + str(value))
+        #value = (weight - 10) / (size * size)
+        user = Female(weight, size)
+        print("Your imc is " + str(user.calc()))
+    #print("Your imc is " + str(value))
 
 def call():
     sex : int = int(input("Are you :\n1 Male\n 2 Female\n"))
